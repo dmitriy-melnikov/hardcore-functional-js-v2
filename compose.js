2acc878797deae3f59a71cbce43cdea7846af473
dzmitriy-melnikov <dimaira2012@yandex.ru>
2020-04-15 18:12:57 +0300

    compose

--- compose.js
+++ compose.js
@@ -1,7 +1,18 @@
-const toUpper = x => x.toUpperCase()
-const exclaim = x => x + '!'
+const { curry, compose } = require('ramda');
+const { log } = console;
+const toUpper = x => x.toUpperCase();
+const exclaim = x => x + '!';
 
-const compose = (f, g) => x => f(g(x))
+//const compose = (f, g) => x => f(g(x));
+const first = xs => xs[0];
 
-const res = compose(exclaim, toUpper)
-console.log(res('hi'))
+const concat = curry((x, y) => y + x);
+
+const logger = curry((tag, x) => (log(tag, x), x));
+
+const res = compose(first, compose(exclaim, toUpper));
+const shout = compose(concat('!'), logger('here:'), toUpper, logger('start:'), first);
+//log(res('hi'));
+const resR = compose(first, exclaim, toUpper);
+//log(resR('cool'));
+log(shout('hello'));
