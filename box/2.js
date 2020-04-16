7bd38d2fa97797323901176cbba104e8165f2d7d
dzmitriy-melnikov <dimaira2012@yandex.ru>
2020-04-16 06:40:06 +0300

    functor

--- box/2.js
+++ box/2.js
@@ -1,4 +1,11 @@
 const Box = require("./1");
+/*const Box = x => (
+	{
+		map: f => Box(f(x)),
+		fold: f => f(x),
+		toString: `Box(${x})`
+	});*/
+
 
 const first = xs => xs[0];
 
@@ -8,5 +15,16 @@ const halfTheFirstLargeNumber = xs => {
   return `The answer is ${answer}`;
 };
 
+const compose = (f, g) => x => Box(x).map(g).fold(f);
+
+const halfTheFirstLargeNumber_ = xs =>
+	Box(xs)
+		.map(xs => xs.filter(x => x >= 20))
+		.map(found => first(found) / 2)
+		.fold(answer => `The answer is ${answer}`);
+
+
 const res = halfTheFirstLargeNumber([1, 4, 50]);
+const res1 = halfTheFirstLargeNumber_([1, 4, 50]);
 console.log(res);
+console.log(res1);
