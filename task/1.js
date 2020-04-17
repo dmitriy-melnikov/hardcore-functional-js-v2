2944bda2209d8949922ca8775a771791329ebed5
dzmitriy-melnikov <dimaira2012@yandex.ru>
2020-04-17 08:35:51 +0300

    task monad

--- task/1.js
+++ task/1.js
@@ -1,1 +1,16 @@
-const {Task} = require('../types')
+const { compose } = require('ramda');
+const { Task } = require('../types');
+const { log, error } = console;
+const logIt = x => {
+	log(`x from Task`, x);
+	return x;
+};
+
+const t1 = Task((rej, res) => res(2))
+	.map(logIt)
+	//.map(two => two + 1)
+	.chain(two => Task.of(two + 1))
+	.map(logIt)
+	.map(three => three * 2);
+
+t1.fork(error, logIt);
--- task/2.js
+++ task/2.js
@@ -1,17 +1,32 @@
-const {Task} = require('../types')
-const fs = require('fs')
+const fs = require('fs');
+const {Task} = require('../types');
+
+const {error, log} = console;
+
+const logIt = x => {
+	log('step ', x);
+	return x;
+};
+
+const readFile = (path, enc) =>
+	Task((rej, res) =>
+		fs.readFile(path, enc, (err, contents) =>
+			err ? rej(err) : res(contents)
+		)
+	);
+
+
+const writeFile = (path, contents) =>
+	Task((rej, res) => fs.writeFile(path, contents, (err, contents) =>
+		err ? rej(err) : res(contents))
+	);
+
 
 const app = () =>
-  fs.readFile('config.json', 'utf-8', (err, contents) => {
-    console.log(err, contents)
-    if(err) throw err
+	readFile('config.json', 'utf-8')
+		.map(contents => contents.replace(/3/g, '7'))
+		.chain(cont => writeFile('config1.json', cont))
+		.map(logIt);
 
-    const newContents = contents.replace(/3/g, '6')
-
-    fs.writeFile('config1.json', newContents, (err, _) => {
-      if(err) throw err
-      console.log('success!')
-    })
-  })
-
-app()
+log(app().fork);
+app().fork(error, log);
