bb1b00c823323c2860965be7c9d446c89378bc4e
dzmitriy-melnikov <dimaira2012@yandex.ru>
2020-04-17 12:41:34 +0300

    transform and monad patterns

--- traversable/1.js
+++ traversable/1.js
@@ -1,16 +1,19 @@
-const fs = require('fs')
-const Task = require('data.task')
-const Either = require('../either')
-const {Right, Left, fromNullable} = Either
-const { List, Map } = require('immutable-ext')
+const fs = require('fs');
+const {Task, Either, Id }= require('../types');
+const {Right, Left, fromNullable} = Either;
+const { List, Map } = require('immutable-ext');
+const {error, log} = console;
 
 const greaterThan5 = x =>
-  x.length > 5 ? Right(x) : Left('not greater than 5')
+  x.length > 5 ? Right(x) : Left('not greater than 5');
 
 const looksLikeEmail = x =>
-  x.match(/@/ig) ? Right(x) : Left('not an email')
+  x.match(/@/ig) ? Right(x) : Left('not an email');
 
-const email = "blahh@yadda.com"
-const res = [greaterThan5, looksLikeEmail].map(v => v(email))
-console.log(res)
+const email = "blahh@yadda.com";
+const res = List([greaterThan5, looksLikeEmail])
+	.traverse(Either.of, v => v(email))
+	.fold(error, x => log(x.toJS()));
+
+log(res);
 
--- traversable/2.js
+++ traversable/2.js
@@ -1,10 +1,16 @@
-const fs = require('fs')
-const Task = require('data.task')
-const { List, Map } = require('immutable-ext')
+const fs = require('fs');
+const {Task, Either, Id }= require('../types');
+const {Right, Left, fromNullable} = Either;
+const { List, Map } = require('immutable-ext');
 
 const httpGet = (path, params) =>
-  Task.of(`${path}: result`)
+  Task.of(`${path}: result`);
 
-const getUser = x => httpGet('/user', {id: x})
-const getTimeline = x => httpGet(`/timeline/${x}`, {})
-const getAds = () => httpGet('/ads', {})
+const getUser = x => httpGet('/user', {id: x});
+const getTimeline = x => httpGet(`/timeline/${x}`, {});
+const getAds = () => httpGet('/ads', {});
+
+//console.log(List([getUser, getTimeline, getAds]).traverse(Task.of, f => f()));
+List([getUser, getTimeline, getAds])
+	.traverse(Task.of, f => f())
+	.fork(console.log, x =>console.log(x.toJS()));
